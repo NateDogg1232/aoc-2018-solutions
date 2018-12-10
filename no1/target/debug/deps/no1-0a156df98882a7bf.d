2dcfbc16c29a60aef5ac7737f833b781f0c1f3b9
NateDogg1232 <nathanielbarragan@gmail.com>
2018-12-09 21:47:16 -0800

    Forgot to clean the dir

--- no1/target/debug/deps/no1-0a156df98882a7bf.d
+++
@@ -1,5 +0,0 @@
-/home/nathaniel/Programs/aoc-2018-solutions/no1/target/debug/deps/no1-0a156df98882a7bf.rmeta: src/main.rs
-
-/home/nathaniel/Programs/aoc-2018-solutions/no1/target/debug/deps/no1-0a156df98882a7bf.d: src/main.rs
-
-src/main.rs:
