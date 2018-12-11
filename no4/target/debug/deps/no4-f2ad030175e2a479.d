36d2d90c58eed982c0e716000625c46f1d4b2393
NateDogg1232 <nathanielbarragan@gmail.com>
2018-12-10 20:20:24 -0800

    Always forgetting to run cargo clean

--- no4/target/debug/deps/no4-f2ad030175e2a479.d
+++
@@ -1,5 +0,0 @@
-/home/nathaniel/Programs/aoc-2018-solutions/no4/target/debug/deps/no4-f2ad030175e2a479.rmeta: src/main.rs
-
-/home/nathaniel/Programs/aoc-2018-solutions/no4/target/debug/deps/no4-f2ad030175e2a479.d: src/main.rs
-
-src/main.rs:
